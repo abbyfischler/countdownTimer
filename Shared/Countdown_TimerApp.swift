0610332b3580ed93add5a838105cbee09c4e78f2
Abby Fischler <abigail.fischler@gmail.com>
2021-07-21 10:58:22 -0700

    Initial Commit

---
+++ Shared/Countdown_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Countdown_TimerApp.swift
+//  Shared
+//
+//  Created by Abby Fischler on 7/21/21.
+//
+
+import SwiftUI
+
+@main
+struct Countdown_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
